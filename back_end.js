819d3d32df97e7f2d74ac8884680eaea0c5b52fc
Jérôme M. Seniuk <seniukjerome29@gmail.com>
2026-08-08 12:55:36 -0400

    Added the getUser function

--- back_end.js
+++ back_end.js
@@ -1,12 +1,56 @@
 const express = require("express"); // Import express
 const mongoose = require("mongoose");
 const app = express(); // Set up express.js
+let data;
+let currentUser;
 
-app.listen(3000, () => console.log('server up on :3000')); // Set up the server
 
+
+
+
+// Connect to mongoose
 mongoose.connect("mongodb+srv://seni0028_db_user:MkhSeyBxTy7PNSe8@assignment2.ncyx0hl.mongodb.net")
     .then(() => console.log("Database connected successfully"))
     .catch((err) => {
         console.log("An error occured when attempting to connect to the database!");
         console.log(" " + err.message)
-    })
+    });
+
+// Set up the structure of the data.
+const userListSchema = new mongoose.Schema({
+    user: {type: String, required: true},
+    todoList: [String]
+});
+const todoListModel = new mongoose.model("todo list", userListSchema);
+
+
+
+
+
+
+/* Adds a new user.
+ * req must have in its body a field called userName
+ */
+function getUser(req) {
+    // Check if a user of the name already exists
+    const user = await todoListModel.findOne({user: req.body.userName});
+    if (users != null) {
+        currentUser = users;
+    } else {
+        // Add the user to the database
+        currentUser = await todoListModel.create({user: req.body.userName});
+    }
+}
+
+// Add a new list item
+// req must have a field in it's body called todo, which is a string containing the todo item.
+function addNewListItem(req) {
+    const updatedUser = await todoListModel.updateOne({user: currentUser.user}, {todoList: (user.todoList.push(req.body.todo))});
+}
+
+
+
+
+
+
+app.listen(3000, () => console.log('server up on :3000')); // Set up the server
